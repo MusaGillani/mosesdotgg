6701e6dc05cfbaa338cae89d506acda2edfc74cf
musa gillani <musagillaniwork@gmail.com>
2023-07-13 01:58:06 +0500

    Added a component to render expandable sections Made Projects a server component again

---
+++ components/expandableSection.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { MdExpandLess, MdExpandMore } from "react-icons/md";
+import { PropsWithChildren, useState } from "react";
+
+import { sigmar } from "@/fonts";
+import useComponentSize from "@/hooks/useComponentSize";
+
+type Props = PropsWithChildren<{ sectionName: string }>;
+
+const ExpandableSection: React.FC<Props> = ({ sectionName, children }) => {
+  const [isExpanded, setIsExpanded] = useState(true);
+  const [ref, contentMaxHeight] = useComponentSize();
+
+  return (
+    <>
+      <div className="mt-10 flex w-full items-center justify-between text-3xl">
+        <h1 className={`${sigmar.className}`}>{sectionName}</h1>
+        <button
+          className="rounded-full border-2 border-slate-300"
+          onClick={() => {
+            if (isExpanded) ref.current!.scrollIntoView();
+            setIsExpanded((prev) => !prev);
+          }}
+        >
+          {isExpanded ? <MdExpandLess /> : <MdExpandMore />}
+        </button>
+      </div>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out`}
+        style={{
+          height: isExpanded ? contentMaxHeight : 0,
+        }}
+      >
+        {isExpanded && (
+          <div ref={ref} className="py-5">
+            {children}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ExpandableSection;
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,49 +1,18 @@
-"use client";
-import { MdExpandLess, MdExpandMore } from "react-icons/md";
-import { useState } from "react";
-
-import { sigmar } from "@/fonts";
+import ExpandableSection from "./expandableSection";
 import ProjectCard from "./projectCard";
-import useComponentSize from "@/hooks/useComponentSize";
 
 const Projects: React.FC = () => {
-  const [isExpanded, setIsExpanded] = useState(true);
-  const [ref, contentMaxHeight] = useComponentSize();
-
   return (
-    <>
-      <div className="mt-10 flex w-full items-center justify-between text-3xl">
-        <h1 className={`${sigmar.className}`}>PROJECTS</h1>
-        <button
-          className="rounded-full border-2 border-slate-300"
-          onClick={() => {
-            if (isExpanded) ref.current!.scrollIntoView();
-            setIsExpanded((prev) => !prev);
-          }}
-        >
-          {isExpanded ? <MdExpandLess /> : <MdExpandMore />}
-        </button>
-      </div>
-      <div
-        className={`overflow-hidden transition-all duration-300 ease-in-out`}
-        style={{
-          height: isExpanded ? contentMaxHeight : 0,
-        }}
-      >
-        {isExpanded && (
-          <div ref={ref} className="py-5">
-            <ProjectCard
-              title="COMPLAINTRONIX"
-              type="Semester Project"
-              description="A Mobile application to register and manage student complaints
+    <ExpandableSection sectionName="PROJECTS">
+      <ProjectCard
+        title="COMPLAINTRONIX"
+        type="Semester Project"
+        description="A Mobile application to register and manage student complaints
             related to their internet connections in GIKI hostels"
-              technologies="Flutter Express Firebase MySQL"
-              github="https://github.com/MusaGillani/complaintronix-flutter"
-            />
-          </div>
-        )}
-      </div>
-    </>
+        technologies="Flutter Express Firebase MySQL"
+        github="https://github.com/MusaGillani/complaintronix-flutter"
+      />
+    </ExpandableSection>
   );
 };
 

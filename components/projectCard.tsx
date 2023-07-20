6e5a958c6ac27ad33a7e3fb558400af6e3fdb046
musa gillani <musagillaniwork@gmail.com>
2023-07-20 11:28:07 +0500

    fixed sizing nd color of project description in small screens

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -37,8 +37,8 @@ const ProjectCard: React.FC<Props> = ({
         >
           {type}
         </p>
-        <div className="right-3/4 w-[30vw] overflow-hidden rounded-lg bg-transparent p-6 shadow-lg md:relative md:m-5 md:bg-slate-200 dark:md:bg-gray-800 dark:md:shadow dark:md:shadow-black lg:w-[20vw]">
-          <p className="text-white dark:text-white md:text-black">
+        <div className="right-3/4 overflow-hidden rounded-lg bg-transparent p-6 shadow-lg md:relative md:m-5 md:w-[30vw] md:bg-slate-200 dark:md:bg-gray-800 dark:md:shadow dark:md:shadow-black lg:w-[20vw]">
+          <p className="text-white dark:text-gray-400 md:text-black dark:md:text-white">
             {description}
           </p>
         </div>

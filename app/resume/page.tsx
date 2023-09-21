3544141b265fe98a7bf56f14b02a4ec04ebddd3a
musa gillani <musagillaniwork@gmail.com>
2023-09-21 08:53:41 +0500

    Resume: - layout - size of page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Resume from "./resume";
 // render PDF and also show download button
 function Page() {
-  return <div />;
+  return (
+    <div className="h-full min-h-screen max-w-full bg-white p-5">
+      <Resume />;
+    </div>
+  );
 }
 
 export default Page;
--- app/resume/resume.tsx
+++ app/resume/resume.tsx
@@ -1,24 +1,34 @@
 import React from "react";
+import styles from "./pdf.modules.css";
 
 function Resume() {
   return (
-    <div>
-      {/* General layout */}
-      {/* 
-        left side with 
-            image,
-            contact details     
-            achievements,
-            hobbies
-     */}
-      {/* 
-        right side - main section
-        Profile/Bio
-        Experience
-        Projects
-        Education
-        skills
-      */}
+    <div className={styles.a4}>
+      {/* content */}
+      <div className="grid grid-cols-3 gap-4 bg-[#EFEFEF] p-[20px] text-black">
+        <div className="col-span-1 border border-red-400">
+          <p>left side</p>
+          {/* 
+              left side with 
+                  image,
+                  contact details     
+                  achievements,
+                  hobbies
+          */}
+        </div>
+
+        <div className="col-span-2  border border-red-400">
+          <p>right side</p>
+          {/* 
+            right side - main section
+              Profile/Bio
+              Experience
+              Projects
+              Education
+              skills
+          */}
+        </div>
+      </div>
     </div>
   );
 }

2935637dd99113041d6b0c2040310e2649a392fa
musa gillani <musagillaniwork@gmail.com>
2023-09-21 10:55:04 +0500

    Resume: - layout using tailwind css - removed grid, using flex - Removed style module - Added a Text component for reuse - Layout for left side column

---
+++ app/resume/Text.tsx
@@ -0,0 +1,24 @@
+import React, { PropsWithChildren } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+import { roboto } from "@/fonts";
+
+const textVariants = cva(roboto.className, {
+  variants: {
+    type: {
+      primary: "font-extrabold",
+      secondary: "",
+      ternary: "",
+    },
+  },
+});
+
+export interface TextProps
+  extends React.ParamHTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof textVariants> {}
+
+const Text: React.FC<PropsWithChildren<TextProps>> = ({ children, type }) => {
+  return <p className={cn(textVariants({ type }))}>{children}</p>;
+};
+
+export default Text;
--- app/resume/resume.tsx
+++ app/resume/resume.tsx
@@ -1,26 +1,58 @@
-import React from "react";
-import styles from "./pdf.modules.css";
+import Image from "next/image";
+import Text from "./Text";
 
+const debugClass = "border-2 border-red-400 border-dashed";
 function Resume() {
   return (
-    <div className={styles.a4}>
-      {/* content */}
-      <div className="grid grid-cols-3 gap-4 bg-[#EFEFEF] p-[20px] text-black">
-        <div className="col-span-1 border border-red-400">
-          <p>left side</p>
-          {/* 
-              left side with 
-                  image,
-                  contact details     
-                  achievements,
-                  hobbies
-          */}
+    <div className="mx-auto h-[297mm] w-[210mm] border bg-white">
+      {/*main content */}
+      <div className="flex gap-4 bg-[#EFEFEF] px-10 py-[20px] text-black">
+        <div className={"w-1/3 "}>
+          {/* image card */}
+          <div className="flex flex-col items-center gap-2 rounded-2xl bg-black py-10">
+            <Image
+              src="https://avatars.githubusercontent.com/u/62158726?v=4"
+              alt="profile image"
+              width={150}
+              height={150}
+              className={" rounded-full border-4 border-solid border-white "}
+            />
+            <div className="py-2" />
+            <p className="text-2xl font-bold text-white">Syed Musa Gillani</p>
+            <p className="text-md text-white">Fullstack Engineer</p>
+          </div>
+          {/* contact details  */}
+          <div className="px-2 py-4">
+            <Text type={"primary"}>Contact Details</Text>
+            <div className="py-0.5" />
+            <Text type={"primary"}>Phone: </Text>
+            <Text type={"secondary"}>+92 323 9810312</Text>
+            <div className="py-0.5" />
+            <Text type={"primary"}>Email: </Text>
+            <Text type={"secondary"}>musagillaniwork@gmail.com</Text>
+            <div className="py-0.5" />
+            <Text type={"primary"}>Github: </Text>
+            <Text type={"secondary"}>github.com/MusaGillani</Text>
+          </div>
+          {/* achievements */}
+          <div className="px-2 py-4">
+            <Text type={"primary"}>Achievements</Text>
+            <Text type={"secondary"}>Achievements 1</Text>
+            <Text type={"secondary"}>Achievements 2</Text>
+            <Text type={"secondary"}>Achievements 3</Text>
+          </div>
+          {/* hobbies */}
+          <div className="px-2 py-4">
+            <Text type={"primary"}>Hobbies</Text>
+            <Text type={"secondary"}>Gaming</Text>
+            <Text type={"secondary"}>Music</Text>
+            <Text type={"secondary"}>Horse Riding</Text>
+          </div>
         </div>
 
-        <div className="col-span-2  border border-red-400">
-          <p>right side</p>
+        {/* right side - main section */}
+        <div className={"w-2/3 " + debugClass}>
           {/* 
-            right side - main section
               Profile/Bio
               Experience
               Projects

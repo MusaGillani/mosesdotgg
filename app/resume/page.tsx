da429bebc072e1ac8bee37a1ea61fae354f52e76
Musa Gillani <musagillaniwork@gmail.com>
2023-09-21 07:31:39 +0500

    component and page file for resume view

---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+// render PDF and also show download button
+function Page() {
+  return <div />;
+}
+
+export default Page;
---
+++ app/resume/resume.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <div>
+      {/* General layout */}
+      {/* 
+        left side with 
+            image,
+            contact details     
+            achievements,
+            hobbies
+     */}
+      {/* 
+        right side - main section
+        Profile/Bio
+        Experience
+        Projects
+        Education
+        skills
+      */}
+    </div>
+  );
+}
+
+export default Resume;

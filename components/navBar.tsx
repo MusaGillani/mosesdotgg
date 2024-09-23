8bae233893135931b23224f0268f02670ac53d0b
Musa Gillani <musagillaniwork@gmail.com>
2024-09-23 16:53:08 +0500

    feat: sticky nav when scrolling

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,22 +1,58 @@
+"use client";
 import { sigmar } from "@/fonts";
 import { cn } from "@/lib/utils";
 import ThemeSwitch from "@/components/themeSwitch";
 import PDFDownloadButton from "./pdfDownloadButton";
+import { useCallback, useEffect, useState } from "react";
+
+const SCROLL_DISTANCE = 20; // additional distance to travel in direction
 
 const NavBar: React.FC<{ className?: string }> = ({ className }) => {
+  const [visible, setVisible] = useState(true);
+  const [lastScrollYPos, setLastScrollYPos] = useState(window.scrollY);
+
+  const handler = useCallback(() => {
+    const currScrollYPos = window.scrollY;
+    // up scroll -> lastScrollYPos > currScrollYPos
+    if (lastScrollYPos > currScrollYPos + SCROLL_DISTANCE) {
+      setVisible(true);
+    }
+    // down scroll -> lastScrollYPos < currScrollYPos
+    if (currScrollYPos > lastScrollYPos + SCROLL_DISTANCE) {
+      setVisible(false);
+    }
+
+    setLastScrollYPos(currScrollYPos);
+  }, [lastScrollYPos]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handler);
+    () => window.removeEventListener("scroll", handler);
+  }, [handler]);
+
   return (
     <nav
       className={cn(
-        "flex w-full items-center justify-between opacity-75",
-        className
+        "fixed z-20 w-full transition-[top] duration-300",
+        visible ? "top-0" : "-top-28",
       )}
     >
-      <p className={`text-3xl text-black dark:text-white ${sigmar.className}`}>
-        MOSES.GG
-      </p>
-      <div className="flex items-center gap-x-3">
-        <ThemeSwitch />
-        <PDFDownloadButton />
+      <div
+        className={cn(
+          "flex w-full items-center justify-between bg-white opacity-75 dark:bg-black",
+          "!pt-14", // remove when banner is removed
+          className,
+        )}
+      >
+        <p
+          className={`text-3xl text-black dark:text-white ${sigmar.className}`}
+        >
+          MOSES.GG
+        </p>
+        <div className="flex items-center gap-x-3">
+          <ThemeSwitch />
+          <PDFDownloadButton />
+        </div>
       </div>
     </nav>
   );

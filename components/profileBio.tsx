2006ab98d7f27bd7451a76786ef8b176f8bc7d14
Musa Gillani <62158726+MusaGillani@users.noreply.github.com>
2024-09-28 16:34:34 +0500

    feat: view update (#14)
    update min height of bio to show on entire screen

--- components/profileBio.tsx
+++ components/profileBio.tsx
@@ -17,43 +17,51 @@ const ProfileBio: React.FC<{ className?: string }> = ({ className }) => {
   const [animate, setAnimate] = useState(false);
 
   return (
-    <div className={cn("mt-80 font-normal", sigmar.className, className)}>
-      <Image
-        src="https://avatars.githubusercontent.com/u/62158726?v=4"
-        alt="profile image"
-        width={150}
-        height={150}
-        onLoad={() => setAnimate(true)}
-        className={`${
-          animate ? "scale-100" : "scale-0"
-        } my-4 rounded-full transition delay-200 ease-in`}
-      />
-      <h1 className={`my-2 text-5xl ${roboto.className}`}>
-        Hi, I&apos;m Musa 👋
-      </h1>
-      <p className="text-4xl">Full stack developer, software engineer</p>
-      {company && (
-        <p className={cn("text-2xl")}>
-          Currently working{" "}
-          <a
-            href={company.website}
-            className="text-discord-purple dark:text-discord-purple"
-            target="_blank"
-          >
-            @{company.name}
-          </a>
-        </p>
+    <div
+      className={cn(
+        "flex min-h-fit flex-col justify-end font-normal sm:min-h-lvh",
+        sigmar.className,
+        className,
       )}
-      <div className="my-2 flex text-4xl">
-        <AnimatedGithub href={`https://${github}`} />
-        <AnimatedTwitter href={twitter} />
-        <AnimatedLinkedIn href={linkedIn} />
-        <AnimatedScroll href="/resume" />
+    >
+      <div className="flex min-h-dvh flex-col justify-end sm:min-h-fit">
+        <Image
+          src="https://avatars.githubusercontent.com/u/62158726?v=4"
+          alt="profile image"
+          width={150}
+          height={150}
+          onLoad={() => setAnimate(true)}
+          className={`${
+            animate ? "scale-100" : "scale-0"
+          } my-4 rounded-full transition delay-200 ease-in`}
+        />
+        <h1 className={`my-2 text-5xl ${roboto.className}`}>
+          Hi, I&apos;m Musa 👋
+        </h1>
+        <p className="text-4xl">Full stack developer, software engineer</p>
+        {company && (
+          <p className={cn("text-2xl")}>
+            Currently working{" "}
+            <a
+              href={company.website}
+              className="text-discord-purple dark:text-discord-purple"
+              target="_blank"
+            >
+              @{company.name}
+            </a>
+          </p>
+        )}
+        <div className="my-2 flex text-4xl">
+          <AnimatedGithub href={`https://${github}`} />
+          <AnimatedTwitter href={twitter} />
+          <AnimatedLinkedIn href={linkedIn} />
+          <AnimatedScroll href="/resume" />
+        </div>
       </div>
-      <div className={cn("text-xl", roboto.className)}>
+      <div className={cn("text-pretty text-xl", roboto.className)}>
         <p>
           I love to learn about software. Always on the hunt for new Open Source
-          Projects to play around with. Eager to work in teams or solo.
+          Projects to play around with. <br /> Eager to work in teams or solo.
         </p>
       </div>
     </div>

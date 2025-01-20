8f5244b4e448aeed43fa6466dc5e6fa0673a6da7
Musa Gillani <musagillaniwork@gmail.com>
2025-01-21 04:57:48 +0500

    update info (#17)

--- constants/info.ts
+++ constants/info.ts
@@ -4,7 +4,4 @@ export const twitter = "https://twitter.com/mosesdotgg";
 export const linkedIn = "https://www.linkedin.com/in/musa-gillani/";
 export const website = "https://www.mosesgg.dev";
 export const github = "https://github.com/MusaGillani";
-export const company: { name: string; website: string } | undefined = {
-  name: "Auxcube",
-  website: "https://www.auxcube.com/",
-};
+export const company: { name: string; website: string } | undefined = undefined;
